df1de887d8cd56168bfebd6f07917fc7d421d859
Yusuke FURUSE <furuse@eom.mech.eng.osaka-u.ac.jp>
2017-03-15 23:36:09 +0900

    Add python folder

---
+++ python/sample.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = 1
+b = 2
+
+def calc(x, y):
+    return x + y
+
+print(calc(a, b))
+
+
+class calulation(object):
+    """docstring for calulation."""
+    def __init__(self, arg):
+        super(calulation, self).__init__()
+        self.arg = arg

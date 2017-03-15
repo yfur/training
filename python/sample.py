e75d89ca49136704d667e604650a15c7bf83bbb6
Yusuke FURUSE <furuse@eom.mech.eng.osaka-u.ac.jp>
2017-03-15 23:44:29 +0900

    Study python class

--- python/sample.py
+++ python/sample.py
@@ -9,8 +9,30 @@ def calc(x, y):
 print(calc(a, b))
 
 
-class calulation(object):
-    """docstring for calulation."""
+class MyClass():
+    """docstring for MyClass."""
     def __init__(self, arg):
-        super(calulation, self).__init__()
+        super(MyClass, self).__init__(
+            N = 6,
+            n = 1,
+        )
         self.arg = arg
+
+model = MyClass()
+
+print(model.N)
+#
+# class MyClass():
+#     def __init__(self):
+#         super(MyChain, self).__init__(
+#             l1 = L.Linear(4, 3),
+#             l2 = L.Linear(3, 3),
+#         )
+#
+#     def __call__(self, x, y):
+#         fv = self.fwd(x, y)
+#         loss = F.mean_squared_error(fv, y)
+#         return loss
+#
+#     def fwd(self, x, y):
+#         return F.sigmoid(self.l1(x))
